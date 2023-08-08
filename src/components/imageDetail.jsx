3075ca706d344d8d2d1b8795c019a753b931c09b
Identity <Igmhdmtz@gmail.com>
2023-08-08 00:27:29 -0600

    deleted files

--- src/components/imageDetail.jsx
+++
@@ -1,9 +0,0 @@
-// // En ./components/imageDetail.js
-
-// import React from "react";
-
-// const ImageDetailPage = () => {
-//   // Contenido del componente ImageDetailPage
-// };
-
-// export default ImageDetailPage;

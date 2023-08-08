b589ef8bcbd44796e519c3da8413183a880ba234
Identity <Igmhdmtz@gmail.com>
2023-08-08 00:13:57 -0600

    initial commit

---
+++ src/components/imageDetail.jsx
@@ -0,0 +1,9 @@
+// // En ./components/imageDetail.js
+
+// import React from "react";
+
+// const ImageDetailPage = () => {
+//   // Contenido del componente ImageDetailPage
+// };
+
+// export default ImageDetailPage;
